be10f7e31c83a779d892773a6823e0736519acb8
notoriousp <pegmickelson+school@gmail.com>
2018-07-23 23:51:33 -0500

    Friend finder app with all functionality working

---
+++ app/data/friends.js
@@ -0,0 +1,41 @@
+// Templated Friends Data
+var friendsData = [
+  {
+    name: "LEGO Elvis",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI5SY4LVroNZl4ijEYWceNfqij-e7NaB50XDmVvF3crqfhZq6fXw",
+    scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+  },
+  {
+    name: "The Church Lady",
+    photo:"https://images.amcnetworks.com/ifc.com/wp-content/uploads/2014/10/Dana-Carvey-church-lady-best.jpg",
+    scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+  },
+  {
+    name: "The One",
+    photo:"https://s-media-cache-ak0.pinimg.com/564x/dd/31/d2/dd31d2dd156204fcf9b6aa1c54455adb.jpg",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    name: "Trinity",
+    photo:"http://vignette3.wikia.nocookie.net/matrix/images/6/67/Trinityfull.jpg/revision/latest?cb=20060803214449",
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  },
+  {
+    name: "Linda Richman",
+    photo:"http://oybay.files.wordpress.com/2006/07/snl101myersrichman010704.jpg",
+    scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+  },
+  {
+    name: "Aunt Linda",
+    photo:"http://www.donnysparrow.com/Sparrow_Sketch/the_DON_SPARROW_Sketchbook/Entries/2011/6/15_The_SNL_Game_files/AuntLinda.jpg",
+    scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+  },
+  {
+    name: "Princess Leia",
+    photo:"http://images.techtimes.com/data/images/full/168082/princess-leia-organa.jpg?w=600",
+    scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+  }
+];
+
+// Export to API Route
+module.exports = friendsData;
